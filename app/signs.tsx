6d93f3c0b7918f6d9f7a5c4f47bc7401fd46379c
Tomáš Wróbel <mail@tomaswrobel.dev>
2024-11-29 23:33:23 +0100

    Updating number of signs

--- app/signs.tsx
+++ app/signs.tsx
@@ -2,7 +2,7 @@
 
 import {useEffect, useState} from "react";
 
-export default async function Signs() {
+export default function Signs() {
 	const [signs, setSigns] = useState<string>();
 	useEffect(() => void fetch("/api").then(r => r.text()).then(setSigns), []);
 	return <p className="text-2xl text-slate-800 dark:text-slate-50 mb-6">{signs}</p>;
